1b521b6525d00922e9ca0a39dfd36227e15ec1ed
Suan Lian <suanl0002@gmail.com>
2023-03-23 02:18:59 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+n = 6;
+function increment(n){
+    n++;
+    return n;
+}
+
+function decrement(n){
+    n--;
+    return n;
+}
+
+function makeInt(n){
+    Number.parseInt("2",10);
+    return n; 
+
+}
+
+function preserveDecimal(x){
+    parseFloat(10.12);
+    return x;
+}
